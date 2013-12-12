6ab367cbe9cbd02f82e14fef6ce2cfda535e28ec
Valent1Polo <me@valentinpolo.fr>
2013-12-12 10:00:48 +0100

    Correction

--- src/civilization/Civilization.java
+++ src/civilization/Civilization.java
@@ -1,10 +1,9 @@
 package civilization;
 
-import javax.media.j3d.Sound;
-
 public class Civilization {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("Hello!");
     }
 }
