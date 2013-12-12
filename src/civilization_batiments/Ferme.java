ecc0472db98b1287ea6a017eb67bf3397f11bd2b
unknown <nducom@gmail.com>
2013-12-12 10:59:04 +0100

    Batiments

---
+++ src/civilization_batiments/Ferme.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package civilization_batiments;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Ferme extends Batiment{
+    
+    public Ferme(){
+        this.tempsConstruction=2;
+    }
+    
+}
---
+++ src/civilization_batiments/Port.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package civilization_batiments;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Port extends Batiment{
+    
+    public Port(){
+        this.tempsConstruction=10;
+    }
+    
+}
