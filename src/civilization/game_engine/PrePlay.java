256e11b23edd269ddf23898e11908482b171a5d7
Valent1Polo <me@valentinpolo.fr>
2014-01-18 12:33:00 +0100

    BAckground sur le preplay

--- src/civilization/game_engine/PrePlay.java
+++ src/civilization/game_engine/PrePlay.java
@@ -1,56 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package civilization.game_engine;
 
-import civilization.Plateau;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.tiled.TiledMap;
 
 /**
  *
  * @author Nicolas
  */
-public class PrePlay extends BasicGameState{
-    
+public class PrePlay extends BasicGameState
+{
     GameButton randomMap;
     GameButton startGame;
-    Image map;
+    Image Background, map;
     StateBasedGame game;
     
-    PrePlay(int state) {
+    PrePlay(int state) 
+    {
         
     }
 
     
     @Override
-    public int getID() {
+    public int getID() 
+    {
         return 1;
     }
 
     @Override
-    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+    public void init(GameContainer container, StateBasedGame game) throws SlickException 
+    {
         this.game=game;
         randomMap = new GameButton(100, 100, new Image("Graphics/Buttons/Generer.png"));
         startGame = new GameButton(100, 200, new Image("Graphics/Buttons/Jouer.png"));
         Game.plateau= civilization.game_engine.mapgenerator.Noise.GenerateMap();
         map = new Image("Graphics/Tileset/gameMap.png");
-       
+        Background = new Image("Graphics/Images/MenuSecondaire.png");
     }
 
     @Override
-    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException 
+    {
+        map.draw(586,355,3);
+        Background.draw(0, 0);
         randomMap.Image.draw(100, 100);
-        startGame.Image.draw(100,200);
-        map.draw(200,100,3);
+        startGame.Image.draw(100, 200);
     }
 
     @Override
